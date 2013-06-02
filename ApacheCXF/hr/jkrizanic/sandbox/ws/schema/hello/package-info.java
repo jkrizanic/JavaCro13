c1d2c1acc7660d338e540a123d9891eae6f31be9
jkrizanic <jkrizanic@gmail.com>
2013-06-02 13:07:23 +0200

    deleted generated wsdl2java classes

--- ApacheCXF/hr/jkrizanic/sandbox/ws/schema/hello/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://ws.sandbox.jkrizanic.hr/schema/Hello/", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package hr.jkrizanic.sandbox.ws.schema.hello;
